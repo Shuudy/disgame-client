72b6dc091f71b85f5ff6bf82e6e95d85f167995c
Shuudy <ancieux.jerome059@gmail.com>
2025-03-09 23:18:15 +0100

    feat(games): fetch and display games from API in Games component

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+
 function Games() {
+    const [games, setGames] = useState([]);
+    const { user } = useAuth();
+
+    useEffect(() => {
+        const fetchGames = async () => {
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/games`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                }
+            );
+            const data = await response.json();
+            setGames(data);
+        };
+        fetchGames();
+    }, []);
+
     return (
         <div>
             <h1>Games</h1>
+            <ul>
+                {games.map((game) => (
+                    <li key={game.id}>{game.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
