3fc83e818ef90d69d4a504fb2d72e04650152548
Shuudy <ancieux.jerome059@gmail.com>
2025-03-14 14:03:11 +0100

    feat(parties): add route to display Parties component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Games from "./components/Games/Games";
 import Login from "./components/Auth/Login";
+import Parties from "./components/Games/Parties";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path="login" element={<Login />} />
                     <Route element={<ProtectedRoute />}>
                         <Route path="games" element={<Games />} />
+                        <Route path="games/:id" element={<Parties />} />
                     </Route>
                 </Routes>
             </AuthProvider>
