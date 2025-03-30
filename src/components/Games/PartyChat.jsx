fc044668a900c5b001a07bf415c1a7abc34eeb84
Shuudy <ancieux.jerome059@gmail.com>
2025-03-30 19:59:42 +0200

    feat(parties): add PartyChat component and enable party selection in Parties

---
+++ src/components/Games/PartyChat.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { io } from "socket.io-client";
+import PropTypes from "prop-types";
+
+function PartyChat({ party }) {
+    const { user } = useAuth();
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        const socketInstance = io(import.meta.env.VITE_SOCKET_URL, {
+            auth: {
+                token: user.token,
+            },
+        });
+
+        setSocket(socketInstance);
+
+        socketInstance.on("connect", () => {
+            console.log("Connected to WebSocket server");
+        });
+    }, [user.token]);
+
+    return (
+        <div>
+            <h2>Chat for {party.name}</h2>
+        </div>
+    );
+}
+
+PartyChat.propTypes = {
+    party: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default PartyChat;
