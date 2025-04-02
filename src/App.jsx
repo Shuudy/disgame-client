e2ec2143adea1fb51268356657e1bbbe2e449883
Shuudy <147620148+Shuudy@users.noreply.github.com>
2025-04-02 19:44:50 +0200

    feat: add NotFound component and route for undefined paths (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Games from "./components/Games/Games";
 import Login from "./components/Auth/Login";
 import Parties from "./components/Games/Parties";
+import NotFound from "./components/Errors/NotFound";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                         <Route path="games" element={<Games />} />
                         <Route path="games/:id" element={<Parties />} />
                     </Route>
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
