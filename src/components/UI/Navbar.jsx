af74c19eaa4da3cde1c74b14b3794025e06177a4
Shuudy <ancieux.jerome059@gmail.com>
2025-04-12 23:18:41 +0200

    feat: replace anchor tags with Link components in Navbar for routing

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -1,18 +1,23 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
     return (
         <div className="navbar">
             <div className="navbar__content">
                 <div className="navbar__logo">Logo Disgame</div>
                 <div className="navbar__auth">
-                    <a href="#" className="navbar__button navbar__button-login">
+                    <Link
+                        to="/login"
+                        className="navbar__button navbar__button-login"
+                    >
                         Se connecter
-                    </a>
-                    <a
-                        href="#"
+                    </Link>
+                    <Link
+                        to="/register"
                         className="navbar__button navbar__button-register"
                     >
                         S'inscrire
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
