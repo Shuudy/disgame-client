5d2cd3b209e76439b08f0e0f6d861b5b42ed49c0
Shuudy <ancieux.jerome059@gmail.com>
2025-04-12 18:42:26 +0200

    feat: implement navbar component and update Home page layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,23 @@
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Welcome to the Home page!</p>
-        </div>
+        <>
+            <div className="navbar">
+                <div className="navbar__content">
+                    <div className="navbar__logo">
+                        Logo Disgame
+                    </div>
+                    <div className="navbar__auth">
+                        <a href="#" className="navbar__button navbar__button-login">Se connecter</a>
+                        <a href="#" className="navbar__button navbar__button-register">S'inscrire</a>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <h1>Home</h1>
+                <p>Welcome to the Home page!</p>
+            </div>
+        </>
     );
 }
 
