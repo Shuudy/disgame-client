48e38a45adabfcfedcf373ebe9ea87ace8f30645
Shuudy <ancieux.jerome059@gmail.com>
2025-04-25 12:37:05 +0200

    docs: enhance navigation and sidebar configuration in VitePress setup

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,28 +1,47 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
+import pkg from "../../package.json";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Disgame",
-  description: "A web application that connects gamers looking to play the same games with others who share similar interests and playstyles.",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Getting Started', link: '/getting-started' },
-    ],
+    title: "Disgame",
+    description:
+        "A web application that connects gamers looking to play the same games with others who share similar interests and playstyles.",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            { text: "Getting Started", link: "/getting-started" },
+            {
+                text: pkg.version,
+                items: [
+                    {
+                        text: "Changelog",
+                        link: "https://github.com/Shuudy/disgame-client/releases",
+                    },
+                ],
+            },
+        ],
 
-    sidebar: [
-      {
-        text: 'Introduction',
-        items: [
-          { text: 'Overview', link: '/introduction' },
-          { text: 'Getting Started', link: '/getting-started' }
-        ]
-      },
-    ],
+        sidebar: [
+            {
+                text: "Introduction",
+                items: [
+                    { text: "Overview", link: "/introduction" },
+                    { text: "Getting Started", link: "/getting-started" },
+                ],
+            },
+        ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/Shuudy/disgame-client', text: 'Client Repository' },
-      { icon: 'github', link: 'https://github.com/Shuudy/disgame-server', text: 'Server Repository' }
-    ]
-  }
-})
+        socialLinks: [
+            {
+                icon: "github",
+                link: "https://github.com/Shuudy/disgame-client",
+                text: "Client Repository",
+            },
+            {
+                icon: "github",
+                link: "https://github.com/Shuudy/disgame-server",
+                text: "Server Repository",
+            },
+        ],
+    },
+});
