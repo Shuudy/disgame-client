b6f2454b6169dcd7663b171bb8ed431c29a3173e
Shuudy <ancieux.jerome059@gmail.com>
2025-05-15 18:52:29 +0200

    feat: implement fetching of popular games from API and update UI accordingly

--- src/components/UI/PopularGames.jsx
+++ src/components/UI/PopularGames.jsx
@@ -1,28 +1,41 @@
+import { useEffect, useState } from "react";
 import PopularGamesCard from "./PopularGamesCard";
 
 function PopularGames() {
-    const games = [
-        {
-            title: "Apex Legends",
-            players: "10,000+ joueurs",
-            image: "https://cdn1.epicgames.com/spt-assets/5dcd88f4e2094a698ebffa43438edc33/apex-legends-1b0ri.jpg",
-        },
-        {
-            title: "Fortnite",
-            players: "50,000+ joueurs",
-            image: "https://cdn2.unrealengine.com/ch6s2-header-image-2012x1132-e85de33a762f.jpg",
-        },
-        {
-            title: "Valorant",
-            players: "20,000+ joueurs",
-            image: "https://img.redbull.com/images/c_limit,w_1500,h_1000/f_auto,q_auto/redbullcom/2020/6/5/ctsejxmdtw9inp8zqqqd/valorant-ameliorer-aim-visee-astuces",
-        },
-        {
-            title: "League of Legends",
-            players: "30,000+ joueurs",
-            image: "https://gaming-cdn.com/images/products/9456/orig/league-of-legends-pc-jeu-cover.jpg?v=1662363312",
-        },
-    ];
+    const [popularGames, setPopularGames] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchPopularGames = async () => {
+            try {
+                const response = await fetch(
+                    `${import.meta.env.VITE_API_URL}/games/popular`,
+                );
+                if (!response.ok) {
+                    throw new Error("Failed to fetch popular games");
+                }
+                const data = await response.json();
+                setPopularGames(data);
+            } catch (error) {
+                setError(
+                    "Failed to fetch popular games. Please try again later.",
+                );
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchPopularGames();
+    }, []);
+
+    if (loading) {
+        return <p>Chargement des jeux populaires...</p>;
+    }
+
+    if (error) {
+        return <p>Erreur : {error}</p>;
+    }
 
     return (
         <div className="populargames">
@@ -39,19 +52,26 @@ function PopularGames() {
                         <div className="populargames__header-subtitle-cta">
                             <a href="#">
                                 Voir tous les jeux
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M9.99984 6L8.58984 7.41L13.1698 12L8.58984 16.59L9.99984 18L15.9998 12L9.99984 6Z"/>
+                                <svg
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path d="M9.99984 6L8.58984 7.41L13.1698 12L8.58984 16.59L9.99984 18L15.9998 12L9.99984 6Z" />
                                 </svg>
                             </a>
                         </div>
                     </div>
                 </div>
                 <div className="populargames__content">
-                    {games.map((game, index) => (
+                    {popularGames.map((game, index) => (
                         <PopularGamesCard
                             key={index}
-                            title={game.title}
-                            players={game.players}
+                            id={game.id}
+                            title={game.name}
+                            players="50,000+ joueurs"
                             image={game.image}
                         />
                     ))}
