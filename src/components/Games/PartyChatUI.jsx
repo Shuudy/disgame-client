86c6bba10a49a382e5d3153d9d461cad665b17ab
Shuudy <ancieux.jerome059@gmail.com>
2025-05-16 19:44:04 +0200

    feat: implement PartyChatUI component with layout and styles

--- src/components/Games/PartyChatUI.jsx
+++ src/components/Games/PartyChatUI.jsx
@@ -1,5 +1,24 @@
 function PartyChatUI() {
-    return <h1>PartyChatUI</h1>
+    return (
+        <div className="party-chat">
+            <div className="party-chat__sidebar party-chat__sidebar-left">
+                Left Sidebar
+            </div>
+            <div className="party-chat__main">
+                <div className="party-chat__messages">
+                    <p>User1: Hello!</p>
+                    <p>User2: Hi there!</p>
+                </div>
+                <div className="party-chat__input">
+                    <input type="text" placeholder="Type a message..." />
+                    <button>Send</button>
+                </div>
+            </div>
+            <div className="party-chat__sidebar party-chat__sidebar-right">
+                Right Sidebar
+            </div>
+        </div>
+    );
 }
 
 export default PartyChatUI;
