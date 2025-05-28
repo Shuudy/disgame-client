8c3cc8c508cfb35110e30a53f6beea9d4628b231
Shuudy <ancieux.jerome059@gmail.com>
2025-05-28 11:27:19 +0200

    feat: add tabs and sections to Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import ProfileOverview from "./ProfileOverview";
+import ProfileGames from "./ProfileGames";
+import ProfileParties from "./ProfileParties";
+
 function Profile() {
+    const [activeTab, setActiveTab] = useState("overview");
+
     return (
         <div className="profile__container">
             <div className="profile__header">
@@ -16,7 +23,9 @@ function Profile() {
                         Membre depuis Mars 2022
                     </div>
                     <div className="profile__header-infos-bio">
-                        Joueur passionné avec un amour pour les FPS compétitifs et les jeux de stratégie. Toujours à la recherche de nouveaux coéquipiers !
+                        Joueur passionné avec un amour pour les FPS compétitifs
+                        et les jeux de stratégie. Toujours à la recherche de
+                        nouveaux coéquipiers !
                     </div>
 
                     <div className="profile__header-infos-badges">
@@ -31,16 +40,16 @@ function Profile() {
                                 <path
                                     d="M12.6666 14V12.6667C12.6666 11.9594 12.3856 11.2811 11.8855 10.781C11.3854 10.281 10.7072 10 9.99992 10H5.99992C5.29267 10 4.6144 10.281 4.1143 10.781C3.6142 11.2811 3.33325 11.9594 3.33325 12.6667V14"
                                     stroke="#6B7280"
-                                    stroke-width="1.33333"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="1.33333"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                                 <path
                                     d="M7.99992 7.33333C9.47268 7.33333 10.6666 6.13943 10.6666 4.66667C10.6666 3.19391 9.47268 2 7.99992 2C6.52716 2 5.33325 3.19391 5.33325 4.66667C5.33325 6.13943 6.52716 7.33333 7.99992 7.33333Z"
                                     stroke="#6B7280"
-                                    stroke-width="1.33333"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="1.33333"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                             </svg>
                             <span>Paris, FR</span>
@@ -56,30 +65,30 @@ function Profile() {
                                 <path
                                     d="M10.6666 14V12.6667C10.6666 11.9594 10.3856 11.2811 9.88554 10.781C9.38544 10.281 8.70716 10 7.99992 10H3.99992C3.29267 10 2.6144 10.281 2.1143 10.781C1.6142 11.2811 1.33325 11.9594 1.33325 12.6667V14"
                                     stroke="#6B7280"
-                                    stroke-width="1.33333"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="1.33333"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                                 <path
                                     d="M5.99992 7.33333C7.47268 7.33333 8.66659 6.13943 8.66659 4.66667C8.66659 3.19391 7.47268 2 5.99992 2C4.52716 2 3.33325 3.19391 3.33325 4.66667C3.33325 6.13943 4.52716 7.33333 5.99992 7.33333Z"
                                     stroke="#6B7280"
-                                    stroke-width="1.33333"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="1.33333"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                                 <path
                                     d="M14.6667 14V12.6667C14.6663 12.0758 14.4697 11.5019 14.1077 11.0349C13.7457 10.5679 13.2388 10.2344 12.6667 10.0867"
                                     stroke="#6B7280"
-                                    stroke-width="1.33333"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="1.33333"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                                 <path
                                     d="M10.6667 2.08667C11.2404 2.23354 11.7488 2.56714 12.1118 3.03488C12.4749 3.50262 12.672 4.07789 12.672 4.67C12.672 5.26212 12.4749 5.83739 12.1118 6.30513C11.7488 6.77287 11.2404 7.10647 10.6667 7.25334"
                                     stroke="#6B7280"
-                                    stroke-width="1.33333"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="1.33333"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                             </svg>
                             <span>63 parties rejointes</span>
@@ -95,16 +104,16 @@ function Profile() {
                                 <path
                                     d="M7.99992 14.6667C11.6818 14.6667 14.6666 11.6819 14.6666 8.00001C14.6666 4.31811 11.6818 1.33334 7.99992 1.33334C4.31802 1.33334 1.33325 4.31811 1.33325 8.00001C1.33325 11.6819 4.31802 14.6667 7.99992 14.6667Z"
                                     stroke="#6B7280"
-                                    stroke-width="1.33333"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="1.33333"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                                 <path
                                     d="M8 4V8L10.6667 9.33333"
                                     stroke="#6B7280"
-                                    stroke-width="1.33333"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="1.33333"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                             </svg>
                             <span>633 heures jouées</span>
@@ -113,11 +122,36 @@ function Profile() {
                 </div>
             </div>
             <div className="profile__tabs">
-                <div className="profile__tabs-item active">Aperçu</div>
-                <div className="profile__tabs-item">Jeux</div>
-                <div className="profile__tabs-item">Parties</div>
+                <div
+                    className={`profile__tabs-item${
+                        activeTab === "overview" ? " active" : ""
+                    }`}
+                    onClick={() => setActiveTab("overview")}
+                >
+                    Aperçu
+                </div>
+                <div
+                    className={`profile__tabs-item${
+                        activeTab === "games" ? " active" : ""
+                    }`}
+                    onClick={() => setActiveTab("games")}
+                >
+                    Jeux
+                </div>
+                <div
+                    className={`profile__tabs-item${
+                        activeTab === "parties" ? " active" : ""
+                    }`}
+                    onClick={() => setActiveTab("parties")}
+                >
+                    Parties
+                </div>
             </div>
-            <div className="profile__content"></div>
+            <div className="profile__content">
+                {activeTab === "overview" && <ProfileOverview />}
+                {activeTab === "games" && <ProfileGames />}
+                {activeTab === "parties" && <ProfileParties />}
+            </div>
         </div>
     );
 }
---
+++ src/components/Profile/ProfileOverview.jsx
@@ -0,0 +1,5 @@
+function ProfileOverview() {
+    return <p>Profile overview</p>;
+}
+
+export default ProfileOverview;
