248d3a8b026c608cfa5fe6fc6e61eb3798a1e11d
Shuudy <ancieux.jerome059@gmail.com>
2025-06-03 14:44:21 +0200

    refactor: improve formatting and structure of ProfileReviews component

--- src/components/Profile/ProfileReviews.jsx
+++ src/components/Profile/ProfileReviews.jsx
@@ -1,49 +1,102 @@
 function ProfileReviews() {
     return (
         <div className="profile__reviews">
-
             <div className="profile__reviews-left">
                 <div className="profile__reviews-left-header">
-                    <div className="profile__reviews-left-header-title">Évaluations</div>
-                    <div className="profile__reviews-left-header-rating">4,6</div>
+                    <div className="profile__reviews-left-header-title">
+                        Évaluations
+                    </div>
+                    <div className="profile__reviews-left-header-rating">
+                        4,6
+                    </div>
                 </div>
-                <div className="profile__reviews-left-subheader">3 commentaires</div>
+                <div className="profile__reviews-left-subheader">
+                    3 commentaires
+                </div>
                 <div className="profile__reviews-left-list">
                     <div className="profile__reviews-left-item">
                         <div className="profile__reviews-left-item-info">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="#fff"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
                                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.73 5.82 22l1.18-7.86-5-4.87 6.91-1L12 2z"></path>
                             </svg>
                             Communication
                         </div>
-                        <div className="profile__reviews-left-item-rating">4,2</div>
+                        <div className="profile__reviews-left-item-rating">
+                            4,2
+                        </div>
                     </div>
                     <div className="profile__reviews-left-item">
                         <div className="profile__reviews-left-item-info">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="#fff"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
                                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.73 5.82 22l1.18-7.86-5-4.87 6.91-1L12 2z"></path>
                             </svg>
                             Sympathie & ambiance
                         </div>
-                        <div className="profile__reviews-left-item-rating">4,2</div>
+                        <div className="profile__reviews-left-item-rating">
+                            4,2
+                        </div>
                     </div>
                     <div className="profile__reviews-left-item">
                         <div className="profile__reviews-left-item-info">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="#fff"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
                                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.73 5.82 22l1.18-7.86-5-4.87 6.91-1L12 2z"></path>
                             </svg>
                             Ponctuel
                         </div>
-                        <div className="profile__reviews-left-item-rating">4,2</div>
+                        <div className="profile__reviews-left-item-rating">
+                            4,2
+                        </div>
                     </div>
                     <div className="profile__reviews-left-item">
                         <div className="profile__reviews-left-item-info">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="#fff"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
                                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.73 5.82 22l1.18-7.86-5-4.87 6.91-1L12 2z"></path>
                             </svg>
                             Fair-play
                         </div>
-                        <div className="profile__reviews-left-item-rating">4,2</div>
+                        <div className="profile__reviews-left-item-rating">
+                            4,2
+                        </div>
                     </div>
                 </div>
                 <div className="profile__reviews-left-footer">
@@ -61,13 +114,17 @@ function ProfileReviews() {
                         </div>
                         <div className="profile__reviews-right-item-infos">
                             <div className="profile__reviews-right-item-header">
-                                <div className="profile__reviews-right-item-header-name">ProGamer123</div>
+                                <div className="profile__reviews-right-item-header-name">
+                                    ProGamer123
+                                </div>
                                 <div className="profile__reviews-right-item-header-ratinginfo">
                                     <span>3,4</span>
                                     <span>il y a 3 jours</span>
                                 </div>
                             </div>
-                            <div className="profile__reviews-right-item-content">test</div>
+                            <div className="profile__reviews-right-item-content">
+                                test
+                            </div>
                         </div>
                     </div>
                     <div className="profile__reviews-right-item">
@@ -76,18 +133,21 @@ function ProfileReviews() {
                         </div>
                         <div className="profile__reviews-right-item-infos">
                             <div className="profile__reviews-right-item-header">
-                                <div className="profile__reviews-right-item-header-name">ProGamer123</div>
+                                <div className="profile__reviews-right-item-header-name">
+                                    ProGamer123
+                                </div>
                                 <div className="profile__reviews-right-item-header-ratinginfo">
                                     <span>3,4</span>
                                     <span>il y a 3 jours</span>
                                 </div>
                             </div>
-                            <div className="profile__reviews-right-item-content">test</div>
+                            <div className="profile__reviews-right-item-content">
+                                test
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
