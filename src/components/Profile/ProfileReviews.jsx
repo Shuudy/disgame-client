7127a95b3010b88c3a00b3f0f320fee54026b8fe
Shuudy <ancieux.jerome059@gmail.com>
2025-06-03 22:29:54 +0200

    feat: enhance ProfileReviews component with star rating display and layout adjustments

--- src/components/Profile/ProfileReviews.jsx
+++ src/components/Profile/ProfileReviews.jsx
@@ -4,7 +4,6 @@ import Modal from "react-modal";
 Modal.setAppElement("#root");
 
 function ProfileReviews() {
-
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const openModal = () => {
@@ -13,6 +12,32 @@ function ProfileReviews() {
 
     const closeModal = () => {
         setIsModalOpen(false);
+    };
+
+    const renderStars = (rating) => {
+        const stars = [];
+        for (let i = 0; i < 5; i++) {
+            stars.push(
+                <svg
+                    key={i}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className={`${
+                        i < rating ? "fill-yellow text-yellow" : "text-gray"
+                    }`}
+                >
+                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+                </svg>,
+            );
+        }
+        return stars;
     };
 
     return (
@@ -117,89 +142,41 @@ function ProfileReviews() {
                 </div>
                 <div className="profile__reviews-left-footer">
                     <div className="profile__reviews-left-separator"></div>
-                    <div className="form__button" onClick={openModal}>+ Ajouter un avis</div>
+                    <div className="form__button" onClick={openModal}>
+                        + Ajouter un avis
+                    </div>
                 </div>
             </div>
 
             <div className="profile__reviews-right">
                 <div className="profile__reviews-right-title">Avis récents</div>
                 <div className="profile__reviews-right-list">
-                    <div className="profile__reviews-right-item">
-                        <div className="profile__reviews-right-picture">
-                            <img src="https://placehold.co/48" alt="User" />
-                        </div>
-                        <div className="profile__reviews-right-item-infos">
-                            <div className="profile__reviews-right-item-header">
-                                <div className="profile__reviews-right-item-header-name">
-                                    ProGamer123
+                    {Array(5)
+                        .fill()
+                        .map((_, index) => (
+                            <div className="profile__reviews-right-item" key={index}>
+                                <div className="profile__reviews-right-picture">
+                                    <img
+                                        src="https://placehold.co/48"
+                                        alt="User"
+                                    />
                                 </div>
-                                <div className="profile__reviews-right-item-header-ratinginfo">
-                                    <span>3,4</span>
-                                    <span>il y a 3 jours</span>
-                                </div>
-                            </div>
-                            <div className="profile__reviews-right-item-content">
-                                test
-                            </div>
-                        </div>
-                    </div>
-                    <div className="profile__reviews-right-item">
-                        <div className="profile__reviews-right-picture">
-                            <img src="https://placehold.co/48" alt="User" />
-                        </div>
-                        <div className="profile__reviews-right-item-infos">
-                            <div className="profile__reviews-right-item-header">
-                                <div className="profile__reviews-right-item-header-name">
-                                    ProGamer123
-                                </div>
-                                <div className="profile__reviews-right-item-header-ratinginfo">
-                                    <span>3,4</span>
-                                    <span>il y a 3 jours</span>
+                                <div className="profile__reviews-right-item-infos">
+                                    <div className="profile__reviews-right-item-header">
+                                        <div className="profile__reviews-right-item-header-name">
+                                            ProGamer123
+                                        </div>
+                                        <div className="profile__reviews-right-item-header-ratinginfo">
+                                            <span>{renderStars(3)}</span>
+                                            <span>il y a 3 jours</span>
+                                        </div>
+                                    </div>
+                                    <div className="profile__reviews-right-item-content">
+                                        test
+                                    </div>
                                 </div>
                             </div>
-                            <div className="profile__reviews-right-item-content">
-                                test
-                            </div>
-                        </div>
-                    </div>
-                    <div className="profile__reviews-right-item">
-                        <div className="profile__reviews-right-picture">
-                            <img src="https://placehold.co/48" alt="User" />
-                        </div>
-                        <div className="profile__reviews-right-item-infos">
-                            <div className="profile__reviews-right-item-header">
-                                <div className="profile__reviews-right-item-header-name">
-                                    ProGamer123
-                                </div>
-                                <div className="profile__reviews-right-item-header-ratinginfo">
-                                    <span>3,4</span>
-                                    <span>il y a 3 jours</span>
-                                </div>
-                            </div>
-                            <div className="profile__reviews-right-item-content">
-                                test
-                            </div>
-                        </div>
-                    </div>
-                    <div className="profile__reviews-right-item">
-                        <div className="profile__reviews-right-picture">
-                            <img src="https://placehold.co/48" alt="User" />
-                        </div>
-                        <div className="profile__reviews-right-item-infos">
-                            <div className="profile__reviews-right-item-header">
-                                <div className="profile__reviews-right-item-header-name">
-                                    ProGamer123
-                                </div>
-                                <div className="profile__reviews-right-item-header-ratinginfo">
-                                    <span>3,4</span>
-                                    <span>il y a 3 jours</span>
-                                </div>
-                            </div>
-                            <div className="profile__reviews-right-item-content">
-                                test
-                            </div>
-                        </div>
-                    </div>
+                        ))}
                 </div>
             </div>
             <Modal
